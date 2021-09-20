277e8255cd1c29058733850dfe27065e8e4edffc
unknown <sebastiansryan@gmail.com>
2021-09-19 22:15:09 -0700

    corrected assignment 1 errors in output, included test case outputs

--- 0-week/Bill/src/bill/Bill.java
+++ 0-week/Bill/src/bill/Bill.java
@@ -10,25 +10,26 @@ public class Bill {
         double price1, price2, price3, item1Total, item2Total, item3Total, itemSubtotal, salesTax;
         Scanner scannerObject = new Scanner(System.in);
 
-        scannerObject.useDelimiter("\n");
         System.out.print("Input name of item 1: ");
-        item1 = scannerObject.next();
+        item1 = scannerObject.nextLine();
         System.out.print("Input quantity of item 1: ");
         quant1 = scannerObject.nextInt();
         System.out.print("Input price of item 1: ");
         price1 = scannerObject.nextDouble();
+        scannerObject.nextLine();
         item1Total = quant1 * price1;
 
         System.out.print("Input name of item 2: ");
-        item2 = scannerObject.next();
+        item2 = scannerObject.nextLine();
         System.out.print("Input quantity of item 2: ");
         quant2 = scannerObject.nextInt();
         System.out.print("Input price of item 2: ");
         price2 = scannerObject.nextDouble();
+        scannerObject.nextLine();
         item2Total = quant2 * price2;
 
         System.out.print("Input name of item 3: ");
-        item3 = scannerObject.next();
+        item3 = scannerObject.nextLine();
         System.out.print("Input quantity of item 3: ");
         quant3 = scannerObject.nextInt();
         System.out.print("Input price of item 3: ");
@@ -38,16 +39,58 @@ public class Bill {
         itemSubtotal = item1Total + item2Total + item3Total;
         salesTax = 0.0625 * itemSubtotal;
 
-        System.out.println("\nYour bill:");
-        System.out.printf("\n%-30s%10s%10s%10s", "Item", "Quantity", "Price", "Total");
-        System.out.printf("\n%-30s%10d%10.2f%10.2f", item1, quant1, price1, item1Total);
-        System.out.printf("\n%-30s%10d%10.2f%10.2f", item2, quant2, price2, item2Total);
-        System.out.printf("\n%-30s%10d%10.2f%10.2f", item3, quant3, price3, item3Total);
-        System.out.println("\n");
+        System.out.printf("%n");
+        System.out.println("Your bill:");
+        System.out.printf("%-30s%10s%10s%10s%n", "Item", "Quantity", "Price", "Total");
+        System.out.printf("%-30s%10d%10.2f%10.2f%n", item1, quant1, price1, item1Total);
+        System.out.printf("%-30s%10d%10.2f%10.2f%n", item2, quant2, price2, item2Total);
+        System.out.printf("%-30s%10d%10.2f%10.2f%n", item3, quant3, price3, item3Total);
 
-        System.out.printf("\n%-20s$%-10.2f", "Subtotal", itemSubtotal);
-        System.out.printf("\n%-20s$%-10.2f", "6.25% sales tax", salesTax);
-        System.out.printf("\n%-20s$%-10.2f", "Total", itemSubtotal + salesTax);
-        System.out.println("\n");
+        System.out.printf("%n");
+        System.out.printf("%-20s$%-10.2f%n", "Subtotal", itemSubtotal);
+        System.out.printf("%-20s$%-10.2f%n", "6.25% sales tax", salesTax);
+        System.out.printf("%-20s$%-10.2f%n", "Total", itemSubtotal + salesTax);
+
+        scannerObject.close();
     }
 }
+// TEST CASE OUTPUT
+// Input name of item 1: lollipops
+// Input quantity of item 1: 10
+// Input price of item 1: 0.50
+// Input name of item 2: diet soda
+// Input quantity of item 2: 3
+// Input price of item 2: 1.25
+// Input name of item 3: chocolate bar
+// Input quantity of item 3: 20
+// Input price of item 3: 0.75
+//
+// Your bill:
+// Item Quantity Price Total
+// lollipops 10 0.50 5.00
+// diet soda 3 1.25 3.75
+// chocolate bar 20 0.75 15.00
+
+// Subtotal $23.75
+// 6.25% sales tax $1.48
+// Total $25.23
+// ----------------------------------
+// Input name of item 1: milk
+// Input quantity of item 1: 1
+// Input price of item 1: 4.99
+// Input name of item 2: eggs
+// Input quantity of item 2: 2
+// Input price of item 2: 6.99
+// Input name of item 3: chips
+// Input quantity of item 3: 5
+// Input price of item 3: 2.99
+//
+// Your bill:
+// Item Quantity Price Total
+// milk 1 4.99 4.99
+// eggs 2 6.99 13.98
+// chips 5 2.99 14.95
+
+// Subtotal $33.92
+// 6.25% sales tax $2.12
+// Total $36.04
--- 0-week/VendingMachine/src/vendingmachine/VendingMachine.java
+++ 0-week/VendingMachine/src/vendingmachine/VendingMachine.java
@@ -1,34 +1,51 @@
 package vendingmachine;
+
 import java.util.Scanner;
 
 public class VendingMachine {
 
     public static void main(String[] args) {
-        int itemCost, quarters, dimes, nickels;
-        quarters = dimes = nickels = 0;
+        int itemCost = 0;
+        int change = 0;
+        int quarters = 0;
+        int dimes = 0;
+        int nickels = 0;
+
         Scanner scannerObject = new Scanner(System.in);
-        
+
         System.out.print("Enter price of item (from 25 cents to a dollar, in 5-cent increments): ");
-    
-        itemCost = (int) scannerObject.nextInt();
-        while (itemCost > 0) {
-            if (itemCost - 25 >= 0) {
+
+        itemCost = scannerObject.nextInt();
+        change = 100 - itemCost;
+        while (change > 0) {
+            if (change - 25 >= 0) {
                 quarters++;
-                itemCost -= 25;
-            }
-            else if (itemCost - 10 >= 0) {
+                change -= 25;
+            } else if (change - 10 >= 0) {
                 dimes++;
-                itemCost -= 10;
-            }
-            else if (itemCost - 5 >= 0) {
+                change -= 10;
+            } else if (change - 5 >= 0) {
                 nickels++;
-                itemCost -= 5;
+                change -= 5;
             }
         }
-        
+
         System.out.println("You bought an item for " + itemCost + " cents and gave me a dollar, so your change is:");
         System.out.println(quarters + " quarter(s)");
         System.out.println(dimes + " dime(s)");
         System.out.println(nickels + " nickel(s)");
     }
 }
+// TEST CASE OUTPUT
+// -------------------------
+// Enter price of item (from 25 cents to a dollar, in 5-cent increments): 45
+// You bought an item for 45 cents and gave me a dollar, so your change is:
+// 2 quarter(s)
+// 0 dime(s)
+// 1 nickel(s)
+// -------------------------
+// Enter price of item (from 25 cents to a dollar, in 5-cent increments): 65
+// You bought an item for 65 cents and gave me a dollar, so your change is:
+// 1 quarter(s)
+// 1 dime(s)
+// 0 nickel(s)
