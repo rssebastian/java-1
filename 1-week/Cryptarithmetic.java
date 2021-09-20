a7c0903b2899dd9f95aac9f48ab21be2791e4938
unknown <sebastiansryan@gmail.com>
2021-09-19 22:21:43 -0700

    added test case outputs for euler and cryptarithmetic

--- 1-week/Cryptarithmetic.java
+++ 1-week/Cryptarithmetic.java
@@ -6,24 +6,21 @@
 // GOOD % 4 == 0, so D must be divisible by 2 to be an even number
 // GOOD must be at least > 0124
 
-
 public class Cryptarithmetic {
     public static void main(String[] args) {
-        for (int t = 0; t<=9; t++) {
-            for (int o = 0; o<=9; o++) {   
+        for (int t = 0; t <= 9; t++) {
+            for (int o = 0; o <= 9; o++) {
                 // Move on if T==O
-                if (t!=o) {
-                    int good = 4*(t*100 + o*10 + o);
+                if (t != o) {
+                    int good = 4 * (t * 100 + o * 10 + o);
                     int d = good % 10;
-                    
-                    // Confirms D is unique from T && O 
+
+                    // Confirms D is unique from T && O
                     if (d != t && d != o) {
                         // Checks if GOOD < 1000 so G can be a leading 0
                         int g = (good % 1000 > 9) ? 0 : good % 1000;
                         // Check if G is unique
-                        if (g != d 
-                        && g != t 
-                        && g != o) {
+                        if (g != d && g != t && g != o) {
                             // Check if O in GOOD matches O in TOO
                             int tempFirstO, tempSecondO;
                             if (g == 0) {
@@ -33,8 +30,7 @@ public class Cryptarithmetic {
                                 tempFirstO = good % 1000 / 100;
                                 tempSecondO = good % 100 / 10;
                             }
-                            if (tempFirstO == tempSecondO 
-                                && tempFirstO == o) {
+                            if (tempFirstO == tempSecondO && tempFirstO == o) {
                                 // Solution found!
                                 System.out.printf("The value of T is %d%n", t);
                                 System.out.printf("The value of O is %d%n", o);
@@ -43,10 +39,18 @@ public class Cryptarithmetic {
                                 System.out.printf("The value of TOO is %d%d%d%n", t, o, o);
                                 System.out.printf("The value of GOOD is %d%n", good);
                             }
-                        }    
+                        }
                     }
                 }
             }
         }
     }
 }
+
+// TEST CASE OUTPUT
+// The value of T is 1
+// The value of O is 6
+// The value of D is 4
+// The value of G is 0
+// The value of TOO is 166
+// The value of GOOD is 664
--- 1-week/Euler.java
+++ 1-week/Euler.java
@@ -4,12 +4,12 @@ public class Euler {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         boolean done = false;
-        
+
         // Keep running until user is done
         while (!done) {
-            System.out.print("To what value of e^x would you like to calculate? ");
+            System.out.print("To what value x of e^x would you like to calculate? ");
             int initialXValue = keyboard.nextInt();
-            
+
             if (initialXValue == 0) {
                 System.out.println("When n == 10, e^0 == 1");
                 System.out.println("When n == 50, e^0 == 1");
@@ -18,33 +18,51 @@ public class Euler {
                 double eValue = 1;
                 double numerator = initialXValue;
                 double nthTerm;
-            
-                // Calculate the nth term 
-                for (int n=1; n<=100; n++) {
-                    if (n==1) {
+
+                // Calculate the nth term
+                for (int n = 1; n <= 100; n++) {
+                    if (n == 1) {
                         eValue += 1;
                     } else {
                         numerator *= initialXValue;
                         nthTerm = numerator / n;
-                        
-                        //Divide by 1 through n instead of calculating the factorial
-                        for (int i=n-1; i>0; i--) {
+
+                        // Divide by 1 through n instead of calculating the factorial
+                        for (int i = n - 1; i > 0; i--) {
                             nthTerm /= i;
                         }
                         eValue += nthTerm;
                     }
-                    if (n==10){
+                    if (n == 10) {
                         System.out.printf("When n == 10, e^%d == %f%n", initialXValue, eValue);
-                    } else if (n==50) {
-                        System.out.printf("When n == 50, e^%d == %f%n", initialXValue, eValue); 
-                    } else if (n==100) {
+                    } else if (n == 50) {
+                        System.out.printf("When n == 50, e^%d == %f%n", initialXValue, eValue);
+                    } else if (n == 100) {
                         System.out.printf("When n == 100, e^%d == %f%n", initialXValue, eValue);
                     }
                 }
                 System.out.print("Would you like to run another calculation? (Y/N): ");
-                if (!keyboard.next().equalsIgnoreCase("Y")) done = true;
-                } 
+                if (!keyboard.next().equalsIgnoreCase("Y"))
+                    done = true;
+            }
         }
         keyboard.close();
     }
 }
+
+// TEST CASE OUTPUT
+// To what value x of e^x would you like to calculate? 1
+// When n == 10, e^1 == 2.718282
+// When n == 50, e^1 == 2.718282
+// When n == 100, e^1 == 2.718282
+// Would you like to run another calculation? (Y/N): y
+// To what value x of e^x would you like to calculate? 2
+// When n == 10, e^2 == 6.388995
+// When n == 50, e^2 == 6.389056
+// When n == 100, e^2 == 6.389056
+// Would you like to run another calculation? (Y/N): y
+// To what value x of e^x would you like to calculate? 3
+// When n == 10, e^3 == 18.079665
+// When n == 50, e^3 == 18.085537
+// When n == 100, e^3 == 18.085537
+// Would you like to run another calculation? (Y/N): n
