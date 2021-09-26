a1925a035e3929539e560cae2afb8e11b7fdfbba
unknown <sebastiansryan@gmail.com>
2021-09-26 16:02:37 -0700

    completed TagMaker and TagMakerTester for Assignment 4, resave

--- 3-week/TagMaker/src/TagMakerTester.java
+++ 3-week/TagMaker/src/TagMakerTester.java
@@ -33,33 +33,52 @@ public class TagMakerTester {
     }
 }
 
-/*
- * TEST CASE OUTPUT Welcome to the conference! Let me get you a name tag. Who
- * are you? John Doe From which organization? Facebook Great! Here's your
- * nametag!
- * ############################################################################
- * ### ANNUAL CONFERENCE ###
- * ############################################################################
- * ### NAME: John Doe ### ### ### ### ORGANIZATION: Facebook ### ### ###
- * ############################################################################
- * Did I get your information wrong? Let me try again. Who are you? Dohn Joe
- * From which organization? Amazon Sorry about that! Here's your nametag!
- * ############################################################################
- * ### ANNUAL CONFERENCE ###
- * ############################################################################
- * ### NAME: Dohn Joe ### ### ### ### ORGANIZATION: Amazon ### ### ###
- * ############################################################################
- * ...Wrong again? Let me try one more time. Who are you? John Joe From which
- * organization? Apple Sigh, here you go. Did I get it right this time?
- * ############################################################################
- * ### ANNUAL CONFERENCE ###
- * ############################################################################
- * ### NAME: John Joe ### ### ### ### ORGANIZATION: Apple ### ### ###
- * ############################################################################
- * I got it wrong again? Here's a blank one so you can do it yourself.
- * ############################################################################
- * ### ANNUAL CONFERENCE ###
- * ############################################################################
- * ### NAME: ### ### ### ### ORGANIZATION: ### ### ###
- * ############################################################################
- */
+/* TEST CASE OUTPUT
+Welcome to the conference! Let me get you a name tag. Who are you?
+John Doe
+From which organization?
+Facebook
+Great! Here's your nametag!
+############################################################################
+###                           ANNUAL CONFERENCE                          ###
+############################################################################
+### NAME: John Doe                                                       ###
+###                                                                      ###
+### ORGANIZATION: Facebook                                               ###
+###                                                                      ###
+############################################################################
+Did I get your information wrong? Let me try again. Who are you?
+Dohn Joe
+From which organization?
+Amazon
+Sorry about that! Here's your nametag!
+############################################################################
+###                           ANNUAL CONFERENCE                          ###
+############################################################################
+### NAME: Dohn Joe                                                       ###
+###                                                                      ###
+### ORGANIZATION: Amazon                                                 ###
+###                                                                      ###
+############################################################################
+...Wrong again? Let me try one more time. Who are you?
+John Joe
+From which organization?
+Apple
+Sigh, here you go. Did I get it right this time?
+############################################################################
+###                           ANNUAL CONFERENCE                          ###
+############################################################################
+### NAME: John Joe                                                       ###
+###                                                                      ###
+### ORGANIZATION: Apple                                                  ###
+###                                                                      ###
+############################################################################
+I got it wrong again? Here's a blank one so you can do it yourself.
+############################################################################
+###                           ANNUAL CONFERENCE                          ###
+############################################################################
+### NAME:                                                                ###
+###                                                                      ###
+### ORGANIZATION:                                                        ###
+###                                                                      ###
+############################################################################*/
