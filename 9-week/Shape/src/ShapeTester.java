096208b0aefb9e6f3914ce841033686b7ddecc84
unknown <sebastiansryan@gmail.com>
2021-11-08 13:17:38 -0800

    added some formatting for ShapeTester

--- 9-week/Shape/src/ShapeTester.java
+++ 9-week/Shape/src/ShapeTester.java
@@ -2,13 +2,20 @@ public class ShapeTester {
     public static void main(String[] args) {
         Shape[] s = new Shape[4];
         s[0] = new Point();
-        s[1] = new Line();
+        s[1] = new Line(new Point(0, 0, 0), new Point(4, 4, 4));
         s[2] = new Circle();
         s[3] = new Sphere();
 
+        String linebreak = "--------------------";
+        // Testing ID Function
+        System.out.println(linebreak);
+        System.out.println(linebreak);
+        System.out.println("Testing ID Functionality...");
         System.out.println(s[0].getID());
         System.out.println(s[1].getID());
         System.out.println(s[2].getID());
         System.out.println(s[3].getID());
+        System.out.println(linebreak);
+
     }
 }
