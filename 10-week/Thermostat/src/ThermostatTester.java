92132aefb3bbf52104840fd3a1692f0ae2ca761c
unknown <sebastiansryan@gmail.com>
2021-11-14 21:44:35 -0800

    completed implementation of Thermostat assignment and exception handling

--- 10-week/Thermostat/src/ThermostatTester.java
+++ 10-week/Thermostat/src/ThermostatTester.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class ThermostatTester {
     public static void main(String[] args) throws TemperatureOutofRange {
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Create your new thermostat! Set the lowest possible temperature: ");
+        System.out.print("Create your new thermostat! \r\nSet the lowest possible temperature: ");
         int low = keyboard.nextInt();
-        System.out.print("\r\nSet the highest possible temperature: ");
+        System.out.print("Set the highest possible temperature: ");
         int high = keyboard.nextInt();
         Thermostat t = new Thermostat(low, high);
-        System.out.printf("%nYour new thermostat has a low of %d and a high of %d.%n", low, high);
+        System.out.printf("Your new thermostat has a low of %d and a high of %d.%n", low, high);
         System.out.print("Please set a temperature: ");
         int temp = keyboard.nextInt();
         try {
             t.setTemperature(temp);
+            System.out.printf("Enjoy relaxing at %d degrees!", temp);
         } catch (TemperatureOutofRange e) {
             System.out.println(e.getMessage());
             secondChance(low, high);
         }
+        keyboard.close();
     }
 
     public static void secondChance(int low, int high) throws TemperatureTooLow, TemperatureTooHigh {
@@ -24,7 +26,7 @@ public class ThermostatTester {
         boolean validTemp = false;
         while (!validTemp) {
             try {
-                System.out.println("Please input a new temperature: ");
+                System.out.print("Please input a new temperature: ");
                 int temp = keyboard.nextInt();
                 if (temp < low) {
                     throw new TemperatureTooLow();
@@ -32,6 +34,7 @@ public class ThermostatTester {
                     throw new TemperatureTooHigh();
                 } else {
                     validTemp = true;
+                    System.out.printf("Enjoy relaxing at %d degrees!", temp);
                 }
             } catch (TemperatureTooLow e) {
                 System.out.println(e.getMessage());
@@ -43,3 +46,27 @@ public class ThermostatTester {
 
     }
 }
+
+// TEST CASE OUTPUT
+// ----------------------------
+// Create your new thermostat!
+// Set the lowest possible temperature: 0
+// Set the highest possible temperature: 100
+// Your new thermostat has a low of 0 and a high of 100.
+// Please set a temperature: 150
+// That temperature is too high for this thermostat!
+// Please input a new temperature: -50
+// That temperature is too low for this thermostat!
+// Please input a new temperature: 50
+// Enjoy relaxing at 50 degrees!
+// ----------------------------
+// Create your new thermostat!
+// Set the lowest possible temperature: -50
+// Set the highest possible temperature: 150
+// Your new thermostat has a low of -50 and a high of 150.
+// Please set a temperature: -100
+// That temperature is too low for this thermostat!
+// Please input a new temperature: 200
+// That temperature is too high for this thermostat!
+// Please input a new temperature: 100
+// Enjoy relaxing at 100 degrees!
